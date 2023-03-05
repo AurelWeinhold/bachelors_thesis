d15766b7ed041805bb348cf2424a9c3c058847e2
Aurel Weinhold <aurel@weinhold.org>
2023-03-05 16:45:07 +0100

    Add protocol definitions

--- src/thesis.h
+++ src/thesis.h
@@ -12,5 +12,35 @@ const struct state_keys state_keys = {
 	.state = "state",
 };
 
+struct prot {
+	int op;
+	int value;
+};
+
+/**
+ * Protocol offsets
+ * 0 |----|----|----|----|
+ *   |        op         |
+ * 4 |----|----|----|----|
+ *   |       value       |
+ * 8 |----|----|----|----|
+ */
+#define PROT_PACKET_SIZE 8
+/**
+ * Offsets to the individual fields
+ */
+enum prot_offset {
+	PROT_OP_OFFSET    = 0,
+	PROT_VALUE_OFFSET = 4,
+};
+/**
+ * OP codes
+ */
+enum protocol_op {
+	PROT_OP_NOOP  = 0,
+	PROT_OP_READ  = 1,
+	PROT_OP_WRITE = 2,
+};
+
 #define THESIS_H
 #endif /* ifndef THESIS_H */
