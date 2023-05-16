b2c9fd6c780b00acaa6d07297b3a10eddefe0785
Aurel Weinhold <aurel@weinhold.org>
2023-05-16 14:54:59 +0200

    Add conditional debug prints

--- src/thesis.bpf.c
+++ src/thesis.bpf.c
@@ -5,6 +5,8 @@
 
 #include "thesis.h"
 
+//#define DEBUG
+
 char LICENSE[] SEC("license") = "GPL";
 
 struct {
@@ -18,15 +20,25 @@ SEC("xdp")
 int
 drop_all(struct xdp_md *ctx)
 {
+#ifdef DEBUG
+	// some spacing helps differentiate between the individual packets
+	bpf_printk("\n\n");
+#endif
+
 	__u32 port = 0;
 
 	void *port_lookup = bpf_map_lookup_elem(&state, &state_keys.port);
-	if (port_lookup)
+	if (port_lookup) {
 		port = *(u32 *)port_lookup;
+	}
 
 	// invalid or no port given
-	if (port == 0)
+	if (port == 0) {
+#ifdef DEBUG
+		bpf_printk("Invalid port: %p", port);
+#endif
 		return XDP_PASS;
+	}
 
 	// declare where data starts and ends
 	void *data     = (void *)(long)ctx->data;
@@ -35,50 +47,99 @@ drop_all(struct xdp_md *ctx)
 	// parse ethernet packet
 	// ethernet packet must not go over `data_end` edge
 	struct ethhdr *eth = data;
-	if ((void *)eth + sizeof(*eth) > data_end)
+	if ((void *)eth + sizeof(*eth) > data_end) {
+#ifdef DEBUG
+		bpf_printk("not an ethernet packet");
+#endif
 		return XDP_PASS;
+	}
 
 	// IPv4 packet must not be go over `data_end` edge
 	struct iphdr *ipv4 = (void *)eth + sizeof(*eth);
-	if ((void *)ipv4 + sizeof(*ipv4) > data_end)
+	if ((void *)ipv4 + sizeof(*ipv4) > data_end) {
+#ifdef DEBUG
+		bpf_printk("Not an ipv4 packet");
+#endif
 		return XDP_PASS;
+	}
+
+#ifdef DEBUG
+	// source and destination ip helps identify packets
+	bpf_printk("source ip: %d", ipv4->saddr);
+	bpf_printk("  dest ip: %d", ipv4->daddr);
+#endif
 
 	// protocol must be TCP
-	if (ipv4->protocol != IPPROTO_TCP)
+	if (ipv4->protocol != IPPROTO_TCP) {
+#ifdef DEBUG
+		bpf_printk("Wrong protocol: %i", ipv4->protocol);
+#endif
 		return XDP_PASS;
+	}
 
 	// TODO(Aurel): parse IPv6 packets
 
 	// TCP packet must not go over `data_end` edge
 	struct tcphdr *tcp = (void *)ipv4 + sizeof(*ipv4);
-	if ((void *)tcp + sizeof(*tcp) > data_end)
+	if ((void *)tcp + sizeof(*tcp) > data_end) {
+#ifdef DEBUG
+		bpf_printk("Not a tcp packet");
+#endif
 		return XDP_PASS;
+	}
 
 	if (tcp->syn || tcp->fin || tcp->rst)
 		return XDP_PASS;
 
+#ifdef DEBUG
+	// source and destination port helps identify packets
+	bpf_printk("source port: %d", bpf_ntohs(tcp->source));
+	bpf_printk("  dest port: %d", bpf_ntohs(tcp->dest));
+#endif
+
 	// destination must be `port` (take care of network and host byte order!)
-	if (bpf_ntohs(tcp->dest) != port)
+	if (bpf_ntohs(tcp->dest) != port) {
+#ifdef DEBUG
+		bpf_printk("Different port");
+#endif
 		return XDP_PASS;
+	}
 
 	void *base = (void *)tcp + sizeof(*tcp);
 
 	// TODO(Aurel): What is in the `data_offset`s bytes? Another header?
 	int data_offset = 12;
 	char *data_base = base + data_offset;
-	if ((void *)(data_base + PROT_PACKET_SIZE) > data_end)
+	if ((void *)(data_base + PROT_PACKET_SIZE) > data_end) {
+#ifdef DEBUG
+		bpf_printk("Not the defined protocol");
+#endif
 		return XDP_PASS;
+	}
 
 	struct prot *request = (struct prot *)data_base;
+#ifdef DEBUG
+	// printing op-code helps identify packets
+	// TODO(Aurel): Print not only numerical
+	bpf_printk("op: %d", request->op);
+#endif
 
 	// only handle read requests
-	if (request->op != PROT_OP_READ)
+	if (request->op != PROT_OP_READ) {
+#ifdef DEBUG
+		bpf_printk("Not a read request");
+#endif
 		return XDP_PASS;
+	}
 
 	// get the state
 	int *state_lookup = bpf_map_lookup_elem(&state, &state_keys.state);
-	if (!state_lookup)
+	if (!state_lookup) {
+#ifdef DEBUG
+		bpf_printk("Failed to look up state");
+#endif
 		return XDP_PASS;
+	}
 
 	/*
 	 * **Only change packet after here!**
@@ -102,8 +163,11 @@ drop_all(struct xdp_md *ctx)
 	tcp->dest       = tcp->source;
 	tcp->source     = dest_port;
 
-	bpf_printk("Read value.\nRerouting packet to %lu:%lu", ipv4->daddr,
+#ifdef DEBUG
+	bpf_printk("Successfully read value");
+	bpf_printk("Rerouting packet to %lu:%lu", ipv4->daddr,
 	           bpf_ntohs(tcp->dest));
+#endif
 
 	return XDP_TX;
 }
