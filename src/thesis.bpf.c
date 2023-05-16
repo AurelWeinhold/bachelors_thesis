f2def5c58b0f77ee0cdfbc13989e90e3fed427ab
Aurel Weinhold <aurel@weinhold.org>
2023-05-16 15:44:28 +0200

    Introduce debug level

--- src/thesis.bpf.c
+++ src/thesis.bpf.c
@@ -5,7 +5,7 @@
 
 #include "thesis.h"
 
-//#define DEBUG
+#define DEBUG 0
 
 char LICENSE[] SEC("license") = "GPL";
 
@@ -20,7 +20,7 @@ SEC("xdp")
 int
 drop_all(struct xdp_md *ctx)
 {
-#ifdef DEBUG
+#if DEBUG > 1
 	// some spacing helps differentiate between the individual packets
 	bpf_printk("\n\n");
 #endif
@@ -34,7 +34,7 @@ drop_all(struct xdp_md *ctx)
 
 	// invalid or no port given
 	if (port == 0) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Invalid port: %p", port);
 #endif
 		return XDP_PASS;
@@ -48,7 +48,7 @@ drop_all(struct xdp_md *ctx)
 	// ethernet packet must not go over `data_end` edge
 	struct ethhdr *eth = data;
 	if ((void *)eth + sizeof(*eth) > data_end) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("not an ethernet packet");
 #endif
 		return XDP_PASS;
@@ -57,13 +57,13 @@ drop_all(struct xdp_md *ctx)
 	// IPv4 packet must not be go over `data_end` edge
 	struct iphdr *ipv4 = (void *)eth + sizeof(*eth);
 	if ((void *)ipv4 + sizeof(*ipv4) > data_end) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Not an ipv4 packet");
 #endif
 		return XDP_PASS;
 	}
 
-#ifdef DEBUG
+#if DEBUG > 1
 	// source and destination ip helps identify packets
 	bpf_printk("source ip: %d", ipv4->saddr);
 	bpf_printk("  dest ip: %d", ipv4->daddr);
@@ -71,7 +71,7 @@ drop_all(struct xdp_md *ctx)
 
 	// protocol must be TCP
 	if (ipv4->protocol != IPPROTO_TCP) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Wrong protocol: %i", ipv4->protocol);
 #endif
 		return XDP_PASS;
@@ -82,7 +82,7 @@ drop_all(struct xdp_md *ctx)
 	// TCP packet must not go over `data_end` edge
 	struct tcphdr *tcp = (void *)ipv4 + sizeof(*ipv4);
 	if ((void *)tcp + sizeof(*tcp) > data_end) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Not a tcp packet");
 #endif
 		return XDP_PASS;
@@ -91,7 +91,7 @@ drop_all(struct xdp_md *ctx)
 	if (tcp->syn || tcp->fin || tcp->rst)
 		return XDP_PASS;
 
-#ifdef DEBUG
+#if DEBUG > 1
 	// source and destination port helps identify packets
 	bpf_printk("source port: %d", bpf_ntohs(tcp->source));
 	bpf_printk("  dest port: %d", bpf_ntohs(tcp->dest));
@@ -99,7 +99,7 @@ drop_all(struct xdp_md *ctx)
 
 	// destination must be `port` (take care of network and host byte order!)
 	if (bpf_ntohs(tcp->dest) != port) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Different port");
 #endif
 		return XDP_PASS;
@@ -111,14 +111,14 @@ drop_all(struct xdp_md *ctx)
 	int data_offset = 12;
 	char *data_base = base + data_offset;
 	if ((void *)(data_base + PROT_PACKET_SIZE) > data_end) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Not the defined protocol");
 #endif
 		return XDP_PASS;
 	}
 
 	struct prot *request = (struct prot *)data_base;
-#ifdef DEBUG
+#if DEBUG > 1
 	// printing op-code helps identify packets
 	// TODO(Aurel): Print not only numerical
 	bpf_printk("op: %d", request->op);
@@ -126,7 +126,7 @@ drop_all(struct xdp_md *ctx)
 
 	// only handle read requests
 	if (request->op != PROT_OP_READ) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Not a read request");
 #endif
 		return XDP_PASS;
@@ -135,7 +135,7 @@ drop_all(struct xdp_md *ctx)
 	// get the state
 	int *state_lookup = bpf_map_lookup_elem(&state, &state_keys.state);
 	if (!state_lookup) {
-#ifdef DEBUG
+#if DEBUG > 1
 		bpf_printk("Failed to look up state");
 #endif
 		return XDP_PASS;
@@ -164,7 +164,7 @@ drop_all(struct xdp_md *ctx)
 	tcp->dest       = tcp->source;
 	tcp->source     = dest_port;
 
-#ifdef DEBUG
+#if DEBUG > 0
 	bpf_printk("Successfully read value");
 	bpf_printk("Rerouting packet to %lu:%lu", ipv4->daddr,
 	           bpf_ntohs(tcp->dest));
