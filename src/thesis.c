a40f9b24957bcc7064bc4357087257df602af5ec
Aurel Weinhold <aurel@weinhold.org>
2023-05-16 15:10:50 +0200

    Move server and eBPF handler into same thread again

--- src/thesis.c
+++ src/thesis.c
@@ -221,96 +221,6 @@ init_socket(int *socket_fd, char *port)
 }
 
 int
-userspace(char *port_str)
-{
-	// fallback port: port
-
-	/*
-	 * NOTE(Aurel): Inspired by [beej's guide to networking in
-	 * C](https://beej.us/guide/bgnet/)
-	 */
-
-	int sockfd = -1, new_fd;
-
-	if (init_socket(&sockfd, port_str) < 0) {
-		fprintf(stderr, "initializing socket failed.\n");
-		if (sockfd >= 0)
-			close(sockfd);
-		exit(EXIT_FAILURE);
-	}
-
-	fd_set primary_fds;
-	fd_set read_fds; // temporary fd set for select
-	int fd_max;      // maximum fd
-
-	// clear both fd_sets
-	FD_ZERO(&read_fds);
-	FD_ZERO(&primary_fds);
-
-	struct sockaddr_storage remoteaddr;
-	socklen_t socklen;
-
-	if (listen(sockfd, MAX_CONNECTIONS) == -1) {
-		perror("listen");
-		exit(3);
-	}
-
-	// add the listener to the master set
-	FD_SET(sockfd, &primary_fds);
-	fd_max = sockfd;
-
-	while (!exiting) {
-		// every connection we accept is in primary which we copy to
-		// read_fds to read from
-		read_fds = primary_fds;
-
-		if (select(fd_max + 1, &read_fds, NULL, NULL, NULL) == -1) {
-			perror("select");
-			exit(4);
-		}
-
-		// check each fd for data to read
-		int read = 0;
-		for (int fd = 0; fd <= fd_max; fd++) {
-			if (FD_ISSET(fd, &read_fds)) {
-				// connection found
-
-				if (fd == sockfd) {
-					// new connection found
-					int new_fd;
-					socklen = sizeof remoteaddr;
-					new_fd  = accept(sockfd, (struct sockaddr *)&remoteaddr,
-					                 &socklen);
-					if (new_fd == -1) {
-						perror("accept");
-					} else {
-						// adding new connection to list
-						FD_SET(new_fd, &primary_fds);
-						if (new_fd > fd_max) {
-							// update max fd
-							fd_max = new_fd;
-						}
-						printf("New connection established @ fd-%d.\n", new_fd);
-					}
-				} else {
-					// already established connection is sending data
-#ifndef DEBUG_EBPF_ONLY
-					read = handle_request(fd, &primary_fds);
-#endif
-				}
-			}
-		}
-		/* TODO(Aurel): When to break out of loop?
-		if (read)
-			break;
-		*/
-	}
-
-	close(sockfd);
-	return 0;
-}
-
-int
 main(int argc, char **argv)
 {
 	struct thesis_bpf *obj;
@@ -361,22 +271,58 @@ main(int argc, char **argv)
 	}
 	shared_state->state = 3012;
 
+	/********************
+	 * Setup the server *
+	 ********************
+	 *
+	 * NOTE(Aurel): Inspired by [beej's guide to networking in
+	 * C](https://beej.us/guide/bgnet/)
+	 */
+	int sockfd = -1, new_fd;
+
+	if (init_socket(&sockfd, port_str) < 0) {
+		fprintf(stderr, "initializing socket failed.\n");
+		if (sockfd >= 0)
+			close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+
+	fd_set primary_fds;
+	fd_set read_fds; // temporary fd set for select
+	int fd_max;      // maximum fd
+
+	// clear both fd_sets
+	FD_ZERO(&read_fds);
+	FD_ZERO(&primary_fds);
+
+	struct sockaddr_storage remoteaddr;
+	socklen_t socklen;
+
+	if (listen(sockfd, MAX_CONNECTIONS) == -1) {
+		perror("listen");
+		exit(3);
+	}
+
+	// add the listener to the master set
+	FD_SET(sockfd, &primary_fds);
+	fd_max = sockfd;
+
 #ifndef DEBUG_USERSPACE_ONLY
-	int pid;
-	if ((pid = fork()) == 0) {
-		// child: eBPF
-		struct bpf_map *state_map = NULL;
+	/**************************
+	 * setup the eBPF program *
+	 **************************/
+	struct bpf_map *state_map = NULL;
 
-		/* Load & verify BPF programs */
-		int err = thesis_bpf__load(obj);
-		if (err) {
-			fprintf(stderr, "Failed to load and verify BPF skeleton\n");
-			goto cleanup;
-		}
+	/* Load & verify BPF programs */
+	int err = thesis_bpf__load(obj);
+	if (err) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
 
-		/* Attach to XDP stage */
-		// struct bpf_link bpf_program__attach_xdp(const struct bpf_program, int ifindex)
-		/*
+	/* Attach to XDP stage */
+	// struct bpf_link bpf_program__attach_xdp(const struct bpf_program, int ifindex)
+	/*
 		 * struct bpf_link {
 		 *		atomic64_t refcnt;
 		 * 		u32 id;
@@ -386,58 +332,93 @@ main(int argc, char **argv)
 		 * 		struct work_struct work;
 		 * };
 		 */
-		struct bpf_link *link =
-				bpf_program__attach_xdp(obj->progs.drop_all, ifindex);
-		if (!link) {
-			fprintf(stderr, "Failed to attach eBPF to XDP.\n");
-			goto cleanup;
-		}
-		fprintf(stderr, "Attached eBPF to XDP stage...\n");
+	struct bpf_link *link =
+			bpf_program__attach_xdp(obj->progs.drop_all, ifindex);
+	if (!link) {
+		fprintf(stderr, "Failed to attach eBPF to XDP.\n");
+		goto cleanup;
+	}
+	fprintf(stderr, "Attached eBPF to XDP stage...\n");
 
-		// NOTE(Aurel): filter needs to be loaded to access appropriate memory
-		state_map = obj->maps.state;
+	// NOTE(Aurel): filter needs to be loaded to access appropriate memory
+	state_map = obj->maps.state;
 
-		// NOTE(Aurel): See header for state map keys
-		// pass port to eBPF program
-		err = bpf_map__update_elem(state_map, &state_keys.port,
-		                           sizeof(state_keys.port), &port,
-		                           sizeof(__u32), 0);
-		if (err) {
-			fprintf(stderr, "Failed updating map writing port. errno %s\n",
-			        strerror(errno));
-			goto cleanup;
-		}
+	// NOTE(Aurel): See header for state map keys
+	// pass port to eBPF program
+	err = bpf_map__update_elem(state_map, &state_keys.port,
+	                           sizeof(state_keys.port), &port, sizeof(__u32),
+	                           0);
+	if (err) {
+		fprintf(stderr, "Failed updating map writing port. errno %s\n",
+		        strerror(errno));
+		goto cleanup;
+	}
 
-		// pass state to eBPF program
-		err = bpf_map__update_elem(state_map, &state_keys.state,
-		                           sizeof(state_keys.state),
-		                           &shared_state->state, sizeof(__u32), 0);
-		if (err) {
-			fprintf(stderr, "Failed updating map writing state. errno %s\n",
-			        strerror(errno));
-			goto cleanup;
-		}
-
-		int n = 0;
-		while (!exiting) {
-			if (err < 0) {
-				//printf("Error scanning for user input: %d\n", err);
-				break;
-			}
-		};
-	cleanup:
-		/* Clean up */
-		thesis_bpf__destroy(obj);
-		fprintf(stderr, "Detached eBPF program.\n");
-		fprintf(stderr, "Resuming to user space only.\n");
-		return err < 0 ? -err : 0;
-
-	} else {
-#endif
-		// parent: userspace
-		userspace(port_str);
-#ifndef DEBUG_USERSPACE_ONLY
-		wait(NULL);
+	// pass state to eBPF program
+	err = bpf_map__update_elem(state_map, &state_keys.state,
+	                           sizeof(state_keys.state), &shared_state->state,
+	                           sizeof(__u32), 0);
+	if (err) {
+		fprintf(stderr, "Failed updating map writing state. errno %s\n",
+		        strerror(errno));
+		goto cleanup;
 	}
 #endif
+
+	/*******************************************
+	 * start listening to incoming connections *
+	 *******************************************/
+	while (!exiting) {
+		// every connection we accept is in primary which we copy to
+		// read_fds to read from
+		read_fds = primary_fds;
+
+		if (select(fd_max + 1, &read_fds, NULL, NULL, NULL) == -1) {
+			perror("select");
+			exit(4);
+		}
+
+		// check each fd for data to read
+		int read = 0;
+		for (int fd = 0; fd <= fd_max; fd++) {
+			if (FD_ISSET(fd, &read_fds)) {
+				// connection found
+
+				if (fd == sockfd) {
+					// new connection found
+					int new_fd;
+					socklen = sizeof remoteaddr;
+					new_fd  = accept(sockfd, (struct sockaddr *)&remoteaddr,
+					                 &socklen);
+					if (new_fd == -1) {
+						perror("accept");
+					} else {
+						// adding new connection to list
+						FD_SET(new_fd, &primary_fds);
+						if (new_fd > fd_max) {
+							// update max fd
+							fd_max = new_fd;
+						}
+						printf("New connection established @ fd-%d.\n", new_fd);
+					}
+				} else {
+					// already established connection is sending data
+#ifndef DEBUG_EBPF_ONLY
+					read = handle_request(fd, &primary_fds);
+#endif
+				}
+			}
+		}
+	}
+
+#ifndef DEBUG_USERSPACE_ONLY
+cleanup:
+	/* Clean up eBPF program */
+	thesis_bpf__destroy(obj);
+	fprintf(stderr, "Detached eBPF program.\n");
+	return err < 0 ? -err : 0;
+#endif
+
+	close(sockfd);
+	return 0;
 }
