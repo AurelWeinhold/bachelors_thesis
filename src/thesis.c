c7a830f55bcbd9044b905c8bd901539f602106d7
Aurel Weinhold <aurel@weinhold.org>
2023-05-27 16:01:23 +0200

    Userspace: Application is based on UDP now

--- src/thesis.c
+++ src/thesis.c
@@ -91,6 +91,53 @@ print_prot(struct prot prot)
 	printf("value: %d\n", prot.value);
 }
 
+int
+update_speed_limit(struct state *state, struct bpf_map *state_map)
+{
+	int err = 0;
+#ifndef DEBUG_USERSPACE_ONLY
+	// read cars from map
+	// NOTE(Aurel): this is not the actual location of the
+	// data, but rather the value copied over
+	uint32_t val;
+	err = bpf_map__lookup_elem(state_map, &state_keys.cars,
+	                           sizeof(state_keys.cars), &val, sizeof(__u32), 0);
+	if (!val || err) {
+		fprintf(stderr, "Failed reading map (cars). errno: %s\n",
+		        strerror(errno));
+		return -1;
+	}
+#endif
+
+	// set speed limit according to function
+	printf("Speed limit: %d\n", state->speed_limit);
+	state->speed_limit = calc_speed_limit(state->cars);
+
+#ifndef DEBUG_USERSPACE_ONLY
+	// update map
+	err = bpf_map__update_elem(state_map, &state_keys.speed_limit,
+	                           sizeof(state_keys.speed_limit),
+	                           &state->speed_limit, sizeof(__u32), 0);
+	if (err) {
+		fprintf(stderr, "Failed updating map (speed_limit). errno: %s\n",
+		        strerror(errno));
+		return -1;
+	}
+#endif
+	return 0;
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *
+get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in *)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
 /**
  * Receives data on the given `socket_fd` and parses it into the given `packet`.
  *
@@ -102,26 +149,26 @@ print_prot(struct prot prot)
  *			 received data.
  */
 int
-receive_prot_packet(int socket_fd, struct prot *packet)
+wait_and_receive_prot_packet(int socket_fd, struct prot *packet,
+                             struct sockaddr_storage *from)
 {
-	printf("receiving...\n");
-	uint8_t buf[BUF_SIZE];
+	printf("waiting and receiving...\n");
+
 	size_t nr_bytes_recv = 0;
-	uint8_t *wr_ptr      = buf;
-	int cur_recv;
-	while (nr_bytes_recv < PROT_PACKET_SIZE) {
-		//         recv(socket_fd, buf,    remaining buf_size,       flags)
-		cur_recv = recv(socket_fd, wr_ptr, BUF_SIZE - nr_bytes_recv, 0);
-		if (cur_recv < 0) {
-			perror("recv");
-			return -1;
-		}
-		if (cur_recv == 0)
-			// All data received
-			return -1;
-		nr_bytes_recv += cur_recv;
-		wr_ptr += cur_recv;
+	uint8_t buf[BUF_SIZE];
+	socklen_t addr_len = sizeof(*from);
+	if ((nr_bytes_recv = recvfrom(socket_fd, buf, 100 - 1, 0,
+	                              (struct sockaddr *)from, &addr_len)) == -1) {
+		perror("recvfrom");
+		exit(1);
 	}
+
+	char s[INET_ADDRSTRLEN];
+	inet_ntop(from->ss_family, get_in_addr((struct sockaddr *)from), s,
+	          sizeof(s));
+
+	printf("listener: got packet from %s\n", s);
+	printf("listener: packet is %zu bytes long\n", nr_bytes_recv);
 
 	if (nr_bytes_recv != PROT_PACKET_SIZE) {
 		printf("error receiving: received %zu bytes\n", nr_bytes_recv);
@@ -129,56 +176,36 @@ receive_prot_packet(int socket_fd, struct prot *packet)
 	}
 
 	// parse packet
-	packet->op = buf[PROT_OP_OFFSET];
+	memcpy(&packet->op, buf + PROT_OP_OFFSET, sizeof(packet->op));
 	memcpy(&packet->value, buf + PROT_VALUE_OFFSET, sizeof(packet->value));
 
 	return 0;
 }
 
 int
-send_prot(int socket_fd, struct prot prot)
+send_speed_limit(int socket_fd, struct state *state, struct sockaddr_storage to)
 {
-	int size = sizeof(prot);
+	struct prot reply = { .op = PROT_OP_REPLY, .value = state->speed_limit };
 
-	char buf[size + 1];
-	memcpy(buf + PROT_OP_OFFSET, &prot.op, 4);
-	memcpy(buf + PROT_VALUE_OFFSET, &prot.value, 4);
+	// marshalling
+	char buf[PROT_PACKET_SIZE + 1];
+	memcpy(buf + PROT_OP_OFFSET, &reply.op, 4);
+	memcpy(buf + PROT_VALUE_OFFSET, &reply.value, 4);
 
-	// loop while not all `size` bytes have been sent
-	long nsent = 0;
-	while (nsent != size) {
-		int bytes;
-		if ((bytes = send(socket_fd, buf + nsent, size - nsent, 0)) < -1) {
-			perror("send");
-			return -1;
-		}
-		nsent += bytes;
-	}
+	// sending
+	sendto(socket_fd, buf, PROT_PACKET_SIZE, 0, (struct sockaddr *)&to,
+	       sizeof(to));
+	// TODO(Aurel): Error handling!
 	return 0;
 }
 
 int
-send_speed_limit(int socket_fd, struct state *state)
+handle_request(int socket_fd, struct prot request, struct sockaddr_storage from,
+               struct state *state, struct bpf_map *state_map)
 {
-	struct prot reply = { .op = PROT_OP_REPLY, .value = state->speed_limit };
-	return send_prot(socket_fd, reply);
-}
-
-int
-handle_request(int fd, fd_set *primary_fds, struct state *state,
-               struct bpf_map *state_map)
-{
-	struct prot request;
-	int err = receive_prot_packet(fd, &request);
-	print_prot(request);
-	if (err < 0) {
-		printf("Error receiving packet\n");
-		return -1;
-	}
-
 	switch (request.op) {
 	case PROT_OP_READ:
-		send_speed_limit(fd, state);
+		send_speed_limit(socket_fd, state, from);
 		break;
 	case PROT_OP_WRITE:
 		// TODO(Aurel): Implement writing the state.
@@ -196,17 +223,16 @@ handle_request(int fd, fd_set *primary_fds, struct state *state,
 #endif
 
 		printf("Updated state: %d\n", state->speed_limit);
-		send_speed_limit(fd, state);
+		send_speed_limit(socket_fd, state, from);
 		break;
 	case PROT_OP_GET_SPEED_LIMIT:
 		state->cars++;
-		send_speed_limit(fd, state);
+		update_speed_limit(state, state_map);
+		send_speed_limit(socket_fd, state, from);
 		printf("New car arrived: %d\n", state->cars);
 		break;
 	case PROT_OP_OUT_OF_RANGE:
 		state->cars--;
-		close(fd);
-		FD_CLR(fd, primary_fds);
 		printf("Car left range: %d\n", state->cars);
 		break;
 	default:;
@@ -217,58 +243,42 @@ handle_request(int fd, fd_set *primary_fds, struct state *state,
 int
 init_socket(int *socket_fd, char *port)
 {
-	struct addrinfo hints, *server_info, *p;
-
-	// hints needs to be filled with all 0 except for ai_family, ai_socktype
-	// and ai_flags in order to be automatically filled by getaddrinfo()
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family   = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags    = AI_PASSIVE;
-
+	struct addrinfo hints, *servinfo, *p;
 	int rv;
-	if ((rv = getaddrinfo(NULL, port, &hints, &server_info)) != 0) {
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family   = AF_INET;    // IPv4
+	hints.ai_socktype = SOCK_DGRAM; // UDP
+	hints.ai_flags    = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return -1;
+		return 1;
 	}
 
-	// loop through all the results and bind to the first possible
-	int status = 0;
-	for (p = server_info; p != NULL; p = p->ai_next) {
-		// trying to get a socket
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
 		if ((*socket_fd = socket(p->ai_family, p->ai_socktype,
 		                         p->ai_protocol)) == -1) {
-			perror("server: socket");
+			perror("listener: socket");
 			continue;
 		}
-		// SO_REUSEADDR: Port may be reused after program stops running
-		int optval = 1;
-		setsockopt(*socket_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
 
-		// trying to bind to that socket
-		if ((status = bind(*socket_fd, p->ai_addr, p->ai_addrlen)) == -1) {
+		if (bind(*socket_fd, p->ai_addr, p->ai_addrlen) == -1) {
 			close(*socket_fd);
-			perror("server: bind");
+			perror("listener: bind");
 			continue;
 		}
-		// socket bound
+
 		break;
 	}
-	freeaddrinfo(server_info);
+
 	if (p == NULL) {
-		fprintf(stderr, "server: failed to connect to socket.\n");
-		return -1;
-	}
-	if (status == -1) {
-		fprintf(stderr, "server: failed to bind to socket.\n");
-		return -1;
+		fprintf(stderr, "listener: failed to bind socket\n");
+		return 2;
 	}
 
-	// preparing for incoming connections
-	if (listen(*socket_fd, 1) == -1) {
-		perror("listen");
-		return -1;
-	}
+	freeaddrinfo(servinfo);
 	return 0;
 }
 
@@ -331,34 +341,14 @@ main(int argc, char **argv)
 	 * NOTE(Aurel): Inspired by [beej's guide to networking in
 	 * C](https://beej.us/guide/bgnet/)
 	 */
-	int sockfd = -1, new_fd;
+	int socket_fd = -1, new_fd;
 
-	if (init_socket(&sockfd, port_str) < 0) {
+	if (init_socket(&socket_fd, port_str) < 0) {
 		fprintf(stderr, "initializing socket failed.\n");
-		if (sockfd >= 0)
-			close(sockfd);
+		if (socket_fd >= 0)
+			close(socket_fd);
 		exit(EXIT_FAILURE);
 	}
-
-	fd_set primary_fds;
-	fd_set read_fds; // temporary fd set for select
-	int fd_max;      // maximum fd
-
-	// clear both fd_sets
-	FD_ZERO(&read_fds);
-	FD_ZERO(&primary_fds);
-
-	struct sockaddr_storage remoteaddr;
-	socklen_t socklen;
-
-	if (listen(sockfd, MAX_CONNECTIONS) == -1) {
-		perror("listen");
-		exit(3);
-	}
-
-	// add the listener to the master set
-	FD_SET(sockfd, &primary_fds);
-	fd_max = sockfd;
 
 	int err                   = 0;
 	struct bpf_map *state_map = NULL;
@@ -433,82 +423,21 @@ main(int argc, char **argv)
 	 * start listening to incoming connections *
 	 *******************************************/
 	printf("Server ready for connections:\n");
+	char buf[100];
 	while (!exiting) {
-		// every connection we accept is in primary which we copy to
-		// read_fds to read from
-		read_fds = primary_fds;
-
-		if (select(fd_max + 1, &read_fds, NULL, NULL, NULL) == -1) {
-			perror("select");
-			exit(4);
+		// TODO(Aurel): When to update speed limit for connections handled by eBPF?
+		struct prot request;
+		struct sockaddr_storage from;
+		err = wait_and_receive_prot_packet(socket_fd, &request, &from);
+		if (err < 0) {
+			printf("Error receiving packet\n");
+			goto cleanup;
 		}
-
-		// check each fd for data to read
-		for (int fd = 0; fd <= fd_max; fd++) {
-			if (FD_ISSET(fd, &read_fds)) {
-				// connection found
-
-				if (fd == sockfd) {
-					// new connection found
-					int new_fd;
-					socklen = sizeof remoteaddr;
-					new_fd  = accept(sockfd, (struct sockaddr *)&remoteaddr,
-					                 &socklen);
-					if (new_fd == -1) {
-						perror("accept");
-					} else {
-						// adding new connection to list
-						FD_SET(new_fd, &primary_fds);
-						if (new_fd > fd_max) {
-							// update max fd
-							fd_max = new_fd;
-						}
-						// calculate speed limit
-
-#ifndef DEBUG_USERSPACE_ONLY
-						// read state from map
-						// NOTE(Aurel): this is not the actual location of the
-						// data, but rather the value copied over
-						uint32_t val;
-						err = bpf_map__lookup_elem(state_map, &state_keys.cars,
-						                           sizeof(state_keys.cars),
-						                           &val, sizeof(__u32), 0);
-						if (!val || err) {
-							fprintf(stderr,
-							        "Failed reading map (cars). errno: %s\n",
-							        strerror(errno));
-							goto cleanup;
-						}
-#endif
-
-						// set speed limit according to function
-						printf("Speed limit: %d\n", state.speed_limit);
-						state.speed_limit = calc_speed_limit(state.cars);
-
-#ifndef DEBUG_USERSPACE_ONLY
-						// update map
-						err = bpf_map__update_elem(
-								state_map, &state_keys.speed_limit,
-								sizeof(state_keys.speed_limit),
-								&state.speed_limit, sizeof(__u32), 0);
-						if (err) {
-							fprintf(stderr,
-							        "Failed updating map (speed_limit). errno: %s\n",
-							        strerror(errno));
-							goto cleanup;
-						}
-#endif
-					}
-				} else {
-					// already established connection is sending data
-#ifndef DEBUG_EBPF_ONLY
-					err = handle_request(fd, &primary_fds, &state, state_map);
-					if (err < 0) {
-						goto cleanup;
-					}
-#endif
-				}
-			}
+		print_prot(request);
+		err = handle_request(socket_fd, request, from, &state, state_map);
+		if (err < 0) {
+			printf("Error handling request\n");
+			goto cleanup;
 		}
 	}
 
@@ -520,6 +449,6 @@ cleanup:
 	return err < 0 ? -err : 0;
 #endif
 
-	close(sockfd);
+	close(socket_fd);
 	return 0;
 }
