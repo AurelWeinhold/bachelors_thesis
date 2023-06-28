60eba5aa7d7d8e87214fe7bcf06163bc87646593
Aurel Weinhold <aurel@weinhold.org>
2023-06-28 22:39:49 +0200

    Adjust timings

--- test/run_until_fail/run_until_fail.sh
+++ test/run_until_fail/run_until_fail.sh
@@ -5,7 +5,8 @@ NR_RUNS=10000
 # client config
 IP="192.168.122.43"
 PORT="8080"
-NR_THREADS=10000
+NR_THREADS_START=100
+NR_THREADS=100000
 NR_PACKETS=1000
 
 
@@ -25,21 +26,27 @@ echo $ARGS > "run_$run/config.txt"
 server="thesis"
 if [[ $# -gt 0 ]]; then
         server="thesis_$1"
+	if [[ "$1" == "ebpf" ]]; then
+		NR_THREADS_START=240
+	elif [[ "$1" == "userspace" ]]; then
+		NR_THREADS_START=120
+	fi
+		
 fi
 client="clock"
 
 i=0
 for (( r=0; r<=$NR_RUNS; r++ ))
 do
-	for (( c=120; c<=$NR_THREADS; c++ ))
+	time for (( c=NR_THREADS_START; c<=$NR_THREADS; c++ ))
 	do
-		timeout 30s \
+		timeout 15s \
 			./build/client/client_$client "run_${run}/${server}_${client}" $ARGS $c $NR_PACKETS $i > /dev/null
 
 		if [[ "${PIPESTATUS[0]}" -eq "0" ]];
 		then
 			i=$((i+NR_THREADS))
-			sleep 5
+			sleep 1
 			continue
 		fi
 		echo $((c -1)) | tee -a "run_${run}/${server}_threads.txt"
